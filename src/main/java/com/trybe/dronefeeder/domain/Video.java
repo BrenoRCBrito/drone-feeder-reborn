82dc5767f429d026170be4e2ca6d857fd52788c0
BrenoRCBrito <brenorcbrito@gmail.com>
2022-09-12 15:00:31 -0300

    feat: :sparkles: add lombok annotations to Video object

--- src/main/java/com/trybe/dronefeeder/domain/Video.java
+++ src/main/java/com/trybe/dronefeeder/domain/Video.java
@@ -1,7 +1,13 @@
 package com.trybe.dronefeeder.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Video {
 
   String fileName;
@@ -10,18 +16,6 @@ public class Video {
 
   public Video(String fileName) {
     this.fileName = fileName;
-  }
-
-  public String getFileName() {
-    return fileName;
-  }
-
-  public void setFileName(String fileName) {
-    this.fileName = fileName;
-  }
-
-  public String getDownloadUrl() {
-    return downloadUrl;
   }
 
   public void setDownloadUrl(String fileName) {
